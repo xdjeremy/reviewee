414b1b83937db5adca2ee62c7e1b017cf2062b97
Jeremy <jeremy.fran@icloud.com>
2022-10-05 20:31:45 +0800

    feat: wip landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				header: "url('/header.png')",
+			},
+		},
 		fontFamily: {
 			sans: ['Inter', 'sans-serif'],
 		},
