e90888f66ece7c1b26ae02f1111fe1636adaaf9e
jeremy <fran.jeremy@protonmail.com>
2022-10-24 19:51:35 +0800

    feat: featured quizzes is now a button

--- components/dashboard/newQuiz/Carousel.item.tsx
+++ components/dashboard/newQuiz/Carousel.item.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import Link from "next/link";
 
 interface Props {
   nextSlide: string;
@@ -21,17 +22,25 @@ const CarouselItem: FC<Props> = ({
 }) => {
   return (
     <div id={slideId} className="carousel-item relative w-full">
-      <div
-        className={
-          "flex w-full flex-col items-center justify-center bg-white py-10 shadow-sm"
-        }
-      >
-        <h3 className={"text-3xl font-semibold"}>{title}</h3>
-        <div className={"mt-4 flex flex-col items-center text-left"}>
-          <span>Average score: {averageScore}%</span>
-          <span>Completed by {userCompleted} users</span>
-        </div>
-      </div>
+      <Link href={`/quiz/${uniqId}`}>
+        <button
+          className={
+            "group flex w-full cursor-pointer flex-col items-center justify-center bg-white py-10 shadow-sm"
+          }
+        >
+          <h3
+            className={
+              "from-purple-500 to-green-400 text-3xl font-semibold group-hover:bg-gradient-to-r group-hover:bg-clip-text group-hover:text-transparent"
+            }
+          >
+            {title}
+          </h3>
+          <div className={"mt-4 flex flex-col items-center text-left"}>
+            <span>Average score: {averageScore}%</span>
+            <span>Completed by {userCompleted} users</span>
+          </div>
+        </button>
+      </Link>
       <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
         <a href={`#${prevSlide}`} className="btn-circle btn">
           ❮
